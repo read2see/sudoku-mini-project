f11d3d058806c49e52bfeba2c1a4594f0b3ff3dc
Husain Habib <habib.h.e@outlook.com>
2026-02-02 15:03:35 +0300

    create endpoints for getting board by loading it from file, and displaying a board after loading it from file

---
+++ src/main/java/com/ga/Sudoku/controller/SudokuController.java
@@ -0,0 +1,30 @@
+package com.ga.sudoku.controller;
+
+import com.ga.sudoku.model.SudokuBoard;
+import com.ga.sudoku.service.SudokuService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SudokuController {
+    private final SudokuService sudokuService;
+
+    public SudokuController(SudokuService sudokuService) {
+        this.sudokuService = sudokuService;
+    }
+
+    @GetMapping("/boards/{boardName}")
+    public SudokuBoard getBoard(@PathVariable String boardName) {
+        return sudokuService.loadPuzzle(boardName);
+    }
+
+    @GetMapping(value = "/display-board/{boardName}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String displayBoard(@PathVariable String boardName) {
+        SudokuBoard board = sudokuService.loadPuzzle(boardName);
+        return board.displayBoard();
+    }
+}
