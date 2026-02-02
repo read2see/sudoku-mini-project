21d9642b13e948ad0a9a3ab8834306bfe3877d18
Husain Habib <habib.h.e@outlook.com>
2026-02-02 14:45:09 +0300

    disable data source config due to no db being used

--- src/main/java/com/ga/Sudoku/SudokuApplication.java
+++ src/main/java/com/ga/Sudoku/SudokuApplication.java
@@ -2,8 +2,9 @@ package com.ga.sudoku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class SudokuApplication {
 
 	public static void main(String[] args) {
