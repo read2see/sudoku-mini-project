f4a8d3fd84cea10a9f20c1c61ad65a894d0cb9e6
Husain Habib <habib.h.e@outlook.com>
2026-02-02 14:43:32 +0300

    fix dependencies

---
+++ src/main/resources/SudokuApplication.java
@@ -0,0 +1,14 @@
+package com.ga.sudoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class SudokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokuApplication.class, args);
+	}
+
+}
